168e646093f57589c87a834f6ae2bc16dc48a00b
cxxxr <g23tlm@gmail.com>
2020-10-22 01:48:55 +0900

    remove useless comment

--- deepl.lisp
+++ deepl.lisp
@@ -190,32 +190,3 @@
 
 (defun languages (client)
   (parse-languages-response (post-request client :languages nil)))
-
-#|
-;; Example usage
-
-(defvar *client* (make-instance 'deepl:client :auth-key "..."))
-
-(deepl:translate *client* "hello" :target-lang :ja)
-=> (#S(DEEPL:TRANSLATION :DETECTED-SOURCE-LANGUAGE :EN :TEXT "こんにちわ"))
-
-(deepl:translate *client* '("hello" "world") :target-lang :ja)
-=> (#S(DEEPL:TRANSLATION :DETECTED-SOURCE-LANGUAGE :EN :TEXT "こんにちわ")
-    #S(DEEPL:TRANSLATION :DETECTED-SOURCE-LANGUAGE :EN :TEXT "せかい"))
-
-(deepl:usage *client*)
-=> #S(DEEPL:USAGE :CHARACTER-COUNT 318 :CHARACTER-LIMIT 1000000)
-
-(deepl:languages *client*)
-=> (#S(DEEPL:LANGUAGE :KEYWORD :DE :NAME "German")
-    #S(DEEPL:LANGUAGE :KEYWORD :EN :NAME "English")
-    #S(DEEPL:LANGUAGE :KEYWORD :ES :NAME "Spanish")
-    #S(DEEPL:LANGUAGE :KEYWORD :FR :NAME "French")
-    #S(DEEPL:LANGUAGE :KEYWORD :IT :NAME "Italian")
-    #S(DEEPL:LANGUAGE :KEYWORD :JA :NAME "Japanese")
-    #S(DEEPL:LANGUAGE :KEYWORD :NL :NAME "Dutch")
-    #S(DEEPL:LANGUAGE :KEYWORD :PL :NAME "Polish")
-    #S(DEEPL:LANGUAGE :KEYWORD :PT :NAME "Portuguese")
-    #S(DEEPL:LANGUAGE :KEYWORD :RU :NAME "Russian")
-    #S(DEEPL:LANGUAGE :KEYWORD :ZH :NAME "Chinese"))
-|#
